d2f7d8ebad4f2dbd1373b34592ee51894d4e757e
Florian Wilkens <gh@1wilkens.org>
2019-06-06 09:25:14 +0200

    [FIX] Group tests for parseHexColor

--- test/util_test.dart
+++ test/util_test.dart
@@ -4,28 +4,30 @@ import 'package:test/test.dart';
 import 'package:kapitalist/util.dart';
 
 void main() {
-  test('parseHexColor parses upper case', () {
-    final c = Color.fromRGBO(42, 42, 42, 1);
+  group('parseHexColor', () {
+    test('parses upper case', () {
+      final c = Color.fromRGBO(42, 42, 42, 1);
 
-    expect(c, Util.parseHexColor("2A2A2A"));
-  });
+      expect(c, Util.parseHexColor("2A2A2A"));
+    });
 
-  test('parseHexColor parses lower case', () {
-    final c = Color.fromRGBO(42, 42, 42, 1);
+    test('parses lower case', () {
+      final c = Color.fromRGBO(42, 42, 42, 1);
 
-    expect(c, Util.parseHexColor("2a2a2a"));
-  });
+      expect(c, Util.parseHexColor("2a2a2a"));
+    });
 
-  test('parseHexColor parses mixed case', () {
-    final c = Color.fromRGBO(42, 42, 42, 1);
+    test('parses mixed case', () {
+      final c = Color.fromRGBO(42, 42, 42, 1);
 
-    expect(c, Util.parseHexColor("2A2a2A"));
-  });
+      expect(c, Util.parseHexColor("2A2a2A"));
+    });
 
-  test('parseHexColor ignores hex prefix', () {
-    final c = Color.fromRGBO(42, 42, 42, 1);
+    test('ignores hex prefix', () {
+      final c = Color.fromRGBO(42, 42, 42, 1);
 
-    expect(c, Util.parseHexColor("0x2A2a2A"));
-    expect(c, Util.parseHexColor("0X2A2a2A"));
+      expect(c, Util.parseHexColor("0x2A2a2A"));
+      expect(c, Util.parseHexColor("0X2A2a2A"));
+    });
   });
 }
