9927f8895f7a8127ef58d325bcf089fdcfc52b9f
Florian Wilkens <gh@1wilkens.org>
2019-06-06 09:23:30 +0200

    [FEAT] Add util_test.dart

--- lib/util.dart
+++ lib/util.dart
@@ -2,9 +2,12 @@ import 'dart:ui';
 
 class Util {
   static Color parseHexColor(String color) {
-    final r = color.substring(0, 2);
-    final g = color.substring(2, 4);
-    final b = color.substring(4, 6);
-    return Color.fromARGB(0, int.parse(r, radix: 16), int.parse(g, radix: 16), int.parse(b, radix: 16));
+    // Strip potential leading prefix
+    final str = color.toLowerCase().replaceAll('0x', '');
+
+    final r = str.substring(0, 2);
+    final g = str.substring(2, 4);
+    final b = str.substring(4, 6);
+    return Color.fromRGBO(int.parse(r, radix: 16), int.parse(g, radix: 16), int.parse(b, radix: 16), 1);
   }
 }
--- test/util_test.dart
+++ test/util_test.dart
@@ -1,1 +1,31 @@
-// XXX: Write sample test for Util.parseHexColors
+import 'dart:ui';
+import 'package:test/test.dart';
+
+import 'package:kapitalist/util.dart';
+
+void main() {
+  test('parseHexColor parses upper case', () {
+    final c = Color.fromRGBO(42, 42, 42, 1);
+
+    expect(c, Util.parseHexColor("2A2A2A"));
+  });
+
+  test('parseHexColor parses lower case', () {
+    final c = Color.fromRGBO(42, 42, 42, 1);
+
+    expect(c, Util.parseHexColor("2a2a2a"));
+  });
+
+  test('parseHexColor parses mixed case', () {
+    final c = Color.fromRGBO(42, 42, 42, 1);
+
+    expect(c, Util.parseHexColor("2A2a2A"));
+  });
+
+  test('parseHexColor ignores hex prefix', () {
+    final c = Color.fromRGBO(42, 42, 42, 1);
+
+    expect(c, Util.parseHexColor("0x2A2a2A"));
+    expect(c, Util.parseHexColor("0X2A2a2A"));
+  });
+}
