d43ae6ccbe2ef164362a4959e73cc5a1584d3103
Florian Wilkens <gh@1wilkens.org>
2020-06-14 01:19:27 +0200

    [FEAT] Setup NavigationAction and add untested navigation to home after onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -9,23 +9,30 @@ import 'package:kapitalist/ui/main_page.dart';
 
 import 'package:client/client.dart';
 
+final navigatorKey = GlobalKey<NavigatorState>();
+
 Future<void> main() async {
   // FIXME: figure out, if we need this here
   WidgetsFlutterBinding.ensureInitialized();
 
+  // Build store
   var store = Store(
     initialState: AppState.initial(),
     persistor: KapitalistPersistor(),
   );
 
+  // Wire up navigator key
+  NavigateAction.setNavigatorKey(navigatorKey);
+
   store.dispatch(InitAction());
-  runApp(KapitalistApp(store));
+  runApp(KapitalistApp(store, navigatorKey));
 }
 
 class KapitalistApp extends StatefulWidget {
   final Store<AppState> store;
+  final GlobalKey<NavigatorState> navKey;
 
-  KapitalistApp(this.store);
+  KapitalistApp(this.store, this.navKey);
 
   @override
   _KapitalistAppState createState() => _KapitalistAppState();
@@ -37,24 +44,25 @@ class _KapitalistAppState extends State<KapitalistApp> {
         store: widget.store,
         child: MaterialApp(
           title: 'Kapitalist',
+          navigatorKey: widget.navKey,
           routes: {
             // TODO: investigate why the keyboard only works when login is home instead of regular route
             KapitalistRoutes.ROOT: (ctx) {
-              print(
+              debugPrint(
                   '#### Building page for ${KapitalistRoutes.ONBOARDING} ####');
               return new OnboardingPageConnector();
             },
             KapitalistRoutes.HOME: (ctx) {
-              print('#### Building page for ${KapitalistRoutes.HOME} ####');
+              debugPrint('#### Building page for ${KapitalistRoutes.HOME} ####');
               return new MainPage();
             },
             KapitalistRoutes.NEW_WALLET: (ctx) {
-              print(
+              debugPrint(
                   '#### Building page for ${KapitalistRoutes.NEW_WALLET} ####');
               return new WalletPage();
             },
             KapitalistRoutes.NEW_TRANSACTION: (ctx) {
-              print(
+              debugPrint(
                   '#### Building page for ${KapitalistRoutes.NEW_TRANSACTION} ####');
               return new TransactionPage();
             },
